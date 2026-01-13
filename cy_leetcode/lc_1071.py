6c110acf25fa288b0ecf12f7d970a6d9cbe7cf33
caoye04 <caoye541@gmail.com>
2026-01-13 15:47:19 +0800

    1

---
+++ cy_leetcode/lc_1071.py
@@ -0,0 +1,8 @@
+# 1071.字符串的最大公因子
+# 对于字符串 s 和 t，只有在 s = t + t + t + ... + t + t（t 自身连接 1 次或多次）时，我们才认定 “t 能除尽 s”。
+# 给定两个字符串 str1 和 str2 。返回 最长字符串 x，要求满足 x 能除尽 str1 且 x 能除尽 str2 。
+
+def solution_1071_version1(s,t):
+    if s+t != t+s:
+        return ""
+    
